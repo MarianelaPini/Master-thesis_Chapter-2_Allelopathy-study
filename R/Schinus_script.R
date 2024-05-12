5ff48ac2b6161a15e8cc5589855fd5ca97d5e819
MarianelaPini <marianelapini@usp.br>
2024-05-12 14:48:24 -0300

    Update

--- R/Schinus_script.R
+++ R/Schinus_script.R
@@ -15,38 +15,120 @@ library(DHARMa)
 #Abrindo os dados
 data<- read.csv(here::here("Data/processed", "Schinus_germination.csv"), sep = ";", header=TRUE)
 data
-#tratamentos como fator
-data$Tratamento<-factor(data$Tratamento,levels = (c("agua","peg","trat25","trat50")))
+#Tratamentos como fator
+data$Tratamento<-factor(data$Tratamento,levels = (c("agua","peg","ext2.5","ext5.0")))
 data$Tratamento
 #Criando a variável resposta do modelo, combinação de sucessos e falhas
-#por tratamento, por bandeja#
+#por Tratamento, por bandeja#
 prop<-cbind(success = data$Sucesso, fail = data$Fracasso)
 prop
+#proporção dos dados de sementes germinadas
+data$propor<-data$Sucesso/data$Numerototal
+data$propor
+mean_propor<- aggregate(propor ~ Tratamento, data = data, FUN = mean)
+mean_propor
+data
 #modelo com o termo simplificado do fator aleatório
 mod.scag2<-glmer(prop ~ Tratamento + (1|Bandeja), data = data, family = "binomial")
 mod.scag2
 summary(mod.scag2)
-#Seleção comparando o modelo com variável tratamento com modelo nulo
+#Seleção comparando o modelo com variável Tratamento com modelo nulo
 #
 mod.scag3<- glmer(prop ~ 1 + (1|Bandeja), data = data, family = "binomial")
 mod.scag3
 summary(mod.scag3)
 #
 anova(mod.scag2, mod.scag3)
-#seleciono o modelo cheio,criando os residous
-residuo <- simulateResiduals(fittedModel = mod.scag2, n = 1000)
-plotSimulatedResiduals(residuo)
+data
+anovasc<-aov(propor ~ Tratamento, data)
+summary(anovasc)
+#teste tukey par a par
+TukeyHSD(anovasc)
 #preditos
 proppred<-(predict(mod.scag2, type="response")) 
 proppred
-prop
-comidos <- ggplot(data = data, aes(x = Tratamento, y = proppred, fill=Tratamento))+
-          geom_boxplot() +
-          geom_jitter(size = .8)+
-          theme_minimal()
-agua<-proppred[c(1,2,3,4,5,6,7,8,9,10)]
-mean(agua)
-comidos
+##transformando odds em proportion
+proportions<-proppred/(1+ proppred)
+proportions
+#including data
+data$predicted_prob <- proportions
+#Calculando as medias por nível
+mean_predictions <- aggregate(predicted_prob ~ Tratamento, data = data, FUN = mean)
+mean_predictions
+sd_predictions<-aggregate(predicted_prob ~ Tratamento, data = data, FUN = sd)
+sd_predictions
+dfgrafico<-merge(mean_predictions,sd_predictions,by="Tratamento")
+dfgrafico
+#nomeando colunas
+colnames(dfgrafico)<-c("Tratamento","media","desvio")
+dfgrafico
+str(dfgrafico)
+dfgrafico$ymin<-dfgrafico$media-dfgrafico$desvio
+dfgrafico$ymax<-dfgrafico$media+dfgrafico$desvio
+dfgrafico
+#grafico
+library(ggbeeswarm)
+library(ggplot2)
+
+ps <- ggplot(data = data, aes(x = Tratamento, y = propor, colour=Tratamento))+
+  scale_y_continuous(name = "Proporção de sementes germinadas")+
+  scale_x_discrete(name = "Tipo de Solução")+
+  geom_beeswarm(size=3, alpha=0.1)+theme_classic()
+  
+help("ggplot")                      
+ps
+# Add segments
+psc <- ps +
+  geom_segment(data = dfgrafico, aes(x = Tratamento, y = ymin, xend = Tratamento, yend = ymax), 
+               color = "black",alpha=0.6, size = 0.6)+
+  geom_text(data = dfgrafico, aes(x = Tratamento, y = ymin, label = "-"), 
+            color = "black", size = 6)+
+  geom_text(data = dfgrafico, aes(x = Tratamento, y = ymax, label = "-"), 
+            color = "black", size = 6,vjust=0.1)
+plotschinus<-psc+geom_point(data=dfgrafico,aes(x=Tratamento,y=media),size=2.5,alpha=1)+
+  geom_text(data = data, aes(x = 1, y = 0.3, label = "a"), 
+            color = "black", size = 3)+
+geom_text(data = data, aes(x = 2, y = 0.1, label = "b"), 
+          color = "black", size = 3)+
+geom_text(data = data, aes(x = 3, y = 0.3, label = "ac"), 
+          color = "black", size = 3)+
+geom_text(data = data, aes(x = 4, y = 0.21, label = "ab"), 
+          color = "black", size = 3)
+plotschinus
+ggsave("plot_schinus.jpg", width=6, height=4.5, dpi=600)
+####
+####
+####
+
+plotschinus
+
+#Tentando com grafico base
+plot(x=NULL,y=NULL,
+     type = "n",xaxt="n",xlim= c(1,4),ylim=c(0,0.6), xlab = "", 
+     ylab = "")
+par (mar = c(3, 6, 2, 3), cex.axis = 1, bty = "l")
+dfgrafico
+dfgrafico$Tratamento<-factor(c("agua","peg","trat25","trat50"))
+dfgrafico$Tratamento
+dev.off()
+#cores
+cores<-c("coral","turquoise","purple","green")
+#Pontos das medias
+points(x = dfgrafico$Tratamento, y = dfgrafico$media, pch = 21, bg = cores, cex = 1.2, lwd = 1.3)
+title (xlab ="agua                     peg                  trat25                    trat 50", line = 0.4, cex.lab = 1)
+# Intervalos de confiança
+segments (x0 = c(1,2,3,4), y0 = dfgrafico$media-dfgrafico$desvio, 
+          x1 = c(1,2,3,4), y1 = dfgrafico$media+dfgrafico$desvio)
+text (x = c(1,2,3,4), y =dfgrafico$media-dfgrafico$desvio, labels = "_") # traço horizontal (bigodinho)
+text (x = c(1,2,3,4), y =dfgrafico$media+dfgrafico$desvio, labels = "_")
+xyplot(jitter(propor) ~ Tratamento, jitter = TRUE,data=data , pch=20 , cex=1 , col=cores,alpha=0.2,
+       
+       panel.xyplot(x =dfgrafico$Tratamento,y = dfgrafico$media, bg = cores, pch = 21)  # Additional points in red with different symbol
+       )
+data
+help("xyplot")
+library("lattice")
+data  
 #modelo cheio com peg como nivel basal
 #
 #Primeiro coloco o peg como nivel basal
@@ -56,7 +138,7 @@ data$Tratamento
 mod.scag2<-glmer(prop ~ Tratamento + (1|Bandeja), data = data, family = "binomial")
 mod.scag2
 summary(mod.scag2)
-#Seleção comparando o modelo com variável tratamento com modelo nulo
+#Seleção comparando o modelo com variável Tratamento com modelo nulo
 #
 mod.scag3<- glmer(prop ~ 1 + (1|Bandeja), data = data, family = "binomial")
 mod.scag3
@@ -77,8 +159,12 @@ fixcoefexp<-exp(fixcoef)
 fixcoefexp
 fixcoeexp<-(c(0.2582851,0.9598174,0.6008590))
 #grafico dados, para exploração
-boxplot(Sucesso~ Tratamento, data = data)
-stripchart(Sucesso ~ Tratamento,
+par
+par (mar = c(3, 6, 2, 3), cex.axis = 1, bty = "l")
+plot(odds~ Tratamento, data = data,ylab = "razão de chance
+(germinadas/não germinadas)")
+text(x = c(1, 2, 3, 4), y = c(1.3,0.3,1.3,1.3), labels = c("a", "b", "a","a"),cex = 0.8)
+stripchart(odds ~ Tratamento,
            data = data,
            method = "jitter",
            pch = 19, col= 1:4,
@@ -92,3 +178,18 @@ ggplot(data = data, aes(y = Sucesso,
   geom_jitter(size = .8)+
   theme_minimal()
 proppred=(predict(model3, type="response")) 
+
+
+# Create a sample data frame
+df <- data.frame(
+  A = 1:5,
+  B = 6:10,
+  C = 11:15
+)
+df
+# Change the position of row 3 to the first position
+row_to_move <- df[3, ]
+df <- rbind(row_to_move, df[-3, ])
+df
+
+
--- R/script_alface.R
+++ R/script_alface.R
@@ -6,27 +6,18 @@ library(lme4)
 library(DHARMa)
 help("lme4")
 #Abrindo os dados
+#Primeiro tamanho alface
 data<- read.csv(here::here("Data/processed", "primeira_tamanho_alface.csv"), sep = ";", header=TRUE)
 data
 #tratamentos como fator
-data$tratamento<-factor(data$tratamento,levels = (c("agua","peg","trat25","trat50")))
+data$tratamento<-factor(data$tratamento,levels = (c("agua","peg","ext2.5","ext5.0")))
 data$tratamento
 data$placa<-as.factor(data$placa)
-#grafico dados, para exploração
-boxplot(tamanho~ tratamento, data = data)
-stripchart(tamanho ~ tratamento,
-           data = data,
-           method = "jitter",
-           pch = 19, col= 1:4,
-           vertical = TRUE,
-           add = TRUE)
-library(ggplot2)
-ggplot(data = data, aes(y = tamanho,
-                        x = tratamento,
-                        fill = tratamento)) +
-  geom_boxplot() +
-  geom_jitter(size = .8)+
-  theme_minimal()
+#Médias dos dados
+mediasdata <- aggregate(tamanho ~ tratamento, data = data, FUN = mean)
+mediasdata
+desviodata<-aggregate(tamanho ~ tratamento, data = data, FUN = sd)
+desviodata
 #Modelo cheio
 mod.alf1<-lmer(tamanho ~ tratamento + (1| placa), data = data)
 summary(mod.alf1)
@@ -35,4 +26,183 @@ mod.alf2<- lmer(tamanho ~ 1 + (1| placa), data = data)
 #Seleção de modelo
 anova(mod.alf1,mod.alf2)
 #Modelo cheio selecionado
+#Comparações das medias, testes par a par, para saber se são diferentes
+anova<-aov(tamanho~tratamento, data)
+anova
+summary(anova)
+TukeyHSD(anova)
+#Preditos do modelo
+preditosmod.alf1<-predict(mod.alf1)
+#Incluindo os preditos nos dados
+data$preditos <- preditosmod.alf1
+data$preditos
+#Calculando as medias por nível
+mean_predictions <- aggregate(preditos ~ tratamento, data = data, FUN = mean)
+mean_predictions
+sd_predictions<-aggregate(preditos ~ tratamento, data = data, FUN = sd)
+sd_predictions
+#resolver, desvio todos iguiais??
+dfgrafico<-merge(mean_predictions,sd_predictions,by="tratamento")
+dfgrafico
+#nomeando colunas
+colnames(dfgrafico)<-c("tratamento","media","desvio")
+dfgrafico
+str(dfgrafico)
+dfgrafico$ymin<-dfgrafico$media-dfgrafico$desvio
+dfgrafico$ymax<-dfgrafico$media+dfgrafico$desvio
+dfgrafico
 
+#grafico para exploração
+library(ggplot2)
+library(ggbeeswarm)
+ggplot(data = data, aes(y = tamanho,
+                        x = tratamento,
+                        fill = tratamento)) +
+  geom_boxplot() +
+  geom_jitter(size = .8)+
+  theme_minimal()
+#grafico com as médias e os dados
+plot <- ggplot(data = data, aes(x = tratamento, y = tamanho, colour=tratamento))+
+  scale_y_continuous(name = "Tamanho das plántulas")+
+  scale_x_discrete(name = "Tipo de Solução")+
+  geom_beeswarm(size=3, alpha=0.1)+
+  theme_classic()
+plot
+dfgrafico
+# Adicionar segmentos e letras das médias
+plott1 <- plot +
+  geom_segment(data = dfgrafico, aes(x = tratamento, y = ymin, xend = tratamento, yend = ymax), 
+               color = "black",alpha=0.8, size = 0.6)+
+  geom_text(data = dfgrafico, aes(x = 2, y = 90, label = "a"),
+                                  color = "black", size = 3)+
+  geom_text(data = dfgrafico, aes(x = 1, y = 80, label = "a"),
+            color = "black", size = 3)+
+  geom_text(data = dfgrafico, aes(x = 3, y = 60, label = "b"),
+            color = "black", size = 3)+
+  geom_text(data = dfgrafico, aes(x = 4, y = 60, label = "b"),
+            color = "black", size = 3)
+plott1
+plott1alface<-plott1+geom_point(data=dfgrafico,aes(x=tratamento,y=media),size=2.5,alpha=1)
+plott1alface
+ggsave("plot_alface_1.jpg", width=6, height=4.5, dpi=600)
+#######
+####
+###
+#Calculando as reduções de tamanho em porcentagem para o texto da dissertação
+##
+#agua é 53.344mm, ext 2.5 é -12.016mm, ext 5% -13.838
+#agua ext 2,5%
+porcext2.5agua<-100/53.344*12.016
+porcext5.agua<-100/53.344*13.838
+porcext5.agua
+#peg, coloco como nivel basal
+data$tratamento<-factor(data$tratamento,levels = (c("peg","agua","ext2.5","ext5.0")))
+data$tratamento
+mod.alfpeg<-lmer(tamanho ~ tratamento + (1| placa), data = data)
+mod.alfpeg
+summary(mod.alfpeg)
+#calculo as porcentagens comparado com PEG para o texto da dissertação
+#peg é 49.922, ext 2.5 -8.594, ext 5 -10.415
+porcext2.5peg<-100/49.922*8.594
+porcext2.5peg
+porcext5peg<-100/49.922*10.415
+porcext5peg
+#Segundo tamanho alface
+#Primeiro tamanho alface
+data<- read.csv(here::here("Data/processed", "segunda_tamanho_alface.csv"), sep = ";", header=TRUE)
+data
+#tratamentos como fator
+data$tratamento<-factor(data$tratamento,levels = (c("agua","peg","ext2.5","ext5.0")))
+data$tratamento
+data$placa<-as.factor(data$placa)
+#Médias dos dados
+mediasdata <- aggregate(tamanho ~ tratamento, data = data, FUN = mean)
+mediasdata
+desviodata<-aggregate(tamanho ~ tratamento, data = data, FUN = sd)
+desviodata
+#Modelo cheio
+mod.alf1<-lmer(tamanho ~ tratamento + (1| placa), data = data)
+summary(mod.alf1)
+#Comparação com modelo nulo
+mod.alf2<- lmer(tamanho ~ 1 + (1| placa), data = data)
+#Seleção de modelo
+anova(mod.alf1,mod.alf2)
+#Modelo cheio selecionado
+#Comparações das medias, testes par a par, para saber se são diferentes
+anova<-aov(tamanho~tratamento, data)
+anova
+summary(anova)
+TukeyHSD(anova)
+#Preditos do modelo
+preditosmod.alf1<-predict(mod.alf1)
+#Incluindo os preditos nos dados
+data$preditos <- preditosmod.alf1
+data$preditos
+#Calculando as medias por nível
+mean_predictions <- aggregate(preditos ~ tratamento, data = data, FUN = mean)
+mean_predictions
+sd_predictions<-aggregate(preditos ~ tratamento, data = data, FUN = sd)
+sd_predictions
+dfgrafico2<-merge(mean_predictions,sd_predictions,by="tratamento")
+dfgrafico2
+#nomeando colunas
+colnames(dfgrafico2)<-c("tratamento","media","desvio")
+dfgrafico2
+str(dfgrafico2)
+dfgrafico2$ymin<-dfgrafico2$media-dfgrafico2$desvio
+dfgrafico2$ymax<-dfgrafico2$media+dfgrafico2$desvio
+dfgrafico2
+#grafico para exploração
+library(ggplot2)
+library(ggbeeswarm)
+ggplot(data = data, aes(y = tamanho,
+                        x = tratamento,
+                        fill = tratamento)) +
+  geom_boxplot() +
+  geom_jitter(size = .8)+
+  theme_classic()
+#grafico com as médias e os dados
+plot <- ggplot(data = data, aes(x = tratamento, y = tamanho, colour=tratamento))+
+  scale_y_continuous(name = "Tamanho das plántulas")+
+  scale_x_discrete(name = "Tipo de Solução")+
+  geom_beeswarm(size=3, alpha=0.1)+
+  theme_classic()
+plot
+dfgrafico2
+# Adicionar segmentos e letras das médias
+plott2 <- plot +
+  geom_segment(data = dfgrafico2, aes(x = tratamento, y = ymin, xend = tratamento, yend = ymax), 
+               color = "black",alpha=0.8, size = 0.6)+
+  geom_text(data = dfgrafico2, aes(x = 2, y = 100, label = "a"),
+            color = "black", size = 3)+
+  geom_text(data = dfgrafico2, aes(x = 1, y = 100, label = "a"),
+            color = "black", size = 3)+
+  geom_text(data = dfgrafico2, aes(x = 3, y = 60, label = "b"),
+            color = "black", size = 3)+
+  geom_text(data = dfgrafico2, aes(x = 4, y = 62, label = "b"),
+            color = "black", size = 3)
+plott2
+plott2alface<-plott2+geom_point(data=dfgrafico2,aes(x=tratamento,y=media),size=2.5,alpha=1)
+plott2alface
+ggsave("plot_alface_2.jpg", width=6, height=4.5, dpi=600)
+#####
+#Calculando as reduções de tamanho em porcentagem para o texto da dissertação
+###
+#agua é 57.778mm, ext 2.5 é -18.872mm, ext 5% -14.782
+#agua ext 2,5%
+porcext2.5agua<-100/57.778*18.872
+porcext2.5agua
+porcext5.agua<-100/57.778*14.782
+porcext5.agua
+#peg, coloco como nivel basal
+data$tratamento<-factor(data$tratamento,levels = (c("peg","agua","ext2.5","ext5.0")))
+data$tratamento
+mod.alfpeg<-lmer(tamanho ~ tratamento + (1| placa), data = data)
+mod.alfpeg
+summary(mod.alfpeg)
+#calculo as porcentagens comparado com PEG para o texto da dissertação
+#peg é 63, ext 2.5 -24.094, ext 5 -20.004
+porcext2.5peg<-100/63*24.094
+porcext2.5peg
+porcext5peg<-100/63*20.004
+porcext5peg
